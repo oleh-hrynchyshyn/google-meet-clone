a0d89149b62cde812d3d14cbcd1ab6e44fa617ff
oleh.hrynchyshyn1 <oleh.hrynchyshyn@popspot.com>
2022-05-08 12:34:27 +0300

    init

---
+++ server/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+	console.log(`server is running on ${PORT} port`);
+});
